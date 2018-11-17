408af00dbea10213d9d0b88a59c7494c1496fb18
Bhashithe Abeysinghe <bhashithe@gmail.com>
2018-11-16 23:16:36 -0500

    fixed checking whether sensor is on or not

--- LBP_protocol.py
+++ LBP_protocol.py
@@ -5,7 +5,7 @@ from matplotlib import animation
 from Sensor import Sensor
 from Target import Target
 
-network_items = [(0,0,4), (3,0), (4,1,3), (5,3), (3,3,2), (1,3)]
+network_items = [(0,0,4), (3,0), (4,1,3), (5,3), (2,1,2), (1,3), (4,5,2)]
 
 def from_items(items):
 	sensors = []
@@ -24,36 +24,67 @@ def create_network(sensors, targets):
 	return sensors, targets
 
 def on_rule(network):
+	"""
+	Will turn on or keep a sensor on if 
+	that sensor is the only sensor which is
+	covering a particular target
+
+	Arguments:
+		network: a tuple in the form of (sensors, targets)
+	"""
 	sensors, targets = network
+
 	for sensor in sensors:
+		# remove the sensor from the network if no battery left
+		if sensor.battery <= 0:
+			sensors.remove(sensor)
+			continue
+
 		for target in sensor.cover:
 			sensors.remove(sensor)
 			for ots in sensors:
-				if target in ots.cover:
+				# target should be covered by an `on` sensor
+				if target in ots.cover and ots.status:
 					break
 			else:
 				sensor.on()
 
 			sensors.append(sensor)
+	return sensors, targets
+
 
 def off_rule(network):
+	"""
+	will turn a off or keep a sensor off if there are
+	other sensors taking care of the cover of this 
+	sensor
+
+	Arguments:
+		network: a tuple in the form of (sensors, targets)
+	"""
+
 	sensors, targets = network
 	for sensor in sensors:
 		os_cover = set() # targets covered by other sensors
 		for target in sensor.cover:
 			sensors.remove(sensor)
 			for ots in sensors:
-				if target in ots.cover:
+				if target in ots.cover and ots.status:
 					os_cover.add(target)
 
 			sensors.append(sensor)
 		if os_cover == sensor.cover:
 			sensor.off()
+	return sensors, targets
 
 def shift(network):
-	on_rule(network)
-	off_rule(network)
+	network = on_rule(network)
+	network = off_rule(network)
 
 	for sensor in network[0]:
 		if sensor.status:
 			sensor.battery -= 1
+		print(sensor)
+		print(sensor.cover)
+
+	return network
